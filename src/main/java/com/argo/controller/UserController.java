0e2fc9f895116f5b1ea513d37d9c9a119c79c75c
IasonInBactria <swjasonyang@163.com>
2023-11-20 18:08:00 +0800

    json传递参数dd

--- src/main/java/com/argo/controller/UserController.java
+++ src/main/java/com/argo/controller/UserController.java
@@ -2,11 +2,13 @@ package com.argo.controller;
 
 
 import com.argo.domain.User;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -30,5 +32,19 @@ public class UserController {
     public String pojoParamForJson(@RequestBody User user){
         System.out.println("pojo(json) 参数传递 user ==>" + user);
         return "{'module': 'pojo for json param'}";
+    }
+
+    @RequestMapping("listPojoParamForJson")
+    @ResponseBody
+    public String listPojoParamForJson(@RequestBody List<User> userList){
+        System.out.println("pojo(json) 参数传递 user ==>" + userList);
+        return "{'module': 'list pojo for json param'}";
+    }
+
+    @RequestMapping("/dateParam")
+    @ResponseBody
+    public String dataParam(@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date date){
+        System.out.println("pojo(json) 参数传递 date ==>" + date);
+        return "{'module': 'date param'}";
     }
 }
